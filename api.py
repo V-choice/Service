7352bda91103cbebf9ba27920637fc7caf530035
Junsu Kim <rlskejdk@naver.com>
2021-11-24 18:35:21 +0900

    page 연결수정

--- api.py
+++ api.py
@@ -2,13 +2,10 @@ from flask import redirect, request, render_template, jsonify, Blueprint, sessio
 from models import User, Post
 from db_connect import db
 from flask_bcrypt import Bcrypt
-import datetime
-
-now = datetime.datetime.utcnow
+from datetime import datetime
 
 board = Blueprint('board',__name__)
 bcrypt = Bcrypt()
-
 
 
 @board.before_app_request
@@ -21,36 +18,39 @@ def load_logged_in_user():
 
 @board.route("/join",methods=["GET","POST"])
 def join():
-    if request.method == 'GET':
-        return render_template('join.html')
+    if session.get('login') is None:
+        if request.method == 'GET':
+            return render_template('join.html')
+        else:
+            user_id = request.form['user_id']
+            user_pw = request.form['user_pw']
+            pw_hash = bcrypt.generate_password_hash(user_pw)
+            
+            user = User(user_id, pw_hash)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"result":"success"})
     else:
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
-        pw_hash = bcrypt.generate_password_hash(user_pw)
-        
-        user = User(user_id, pw_hash)
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({"result":"success"})
+        return redirect('/visual')
 
 @board.route('/login',methods = ['GET','POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-        
-    else:
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
-        user = User.query.filter(User.user_id==user_id).first()
-        
-        if user is not None:
-            if bcrypt.check_password_hash(user.user_pw,user_pw):
-                session['login']=user.id
-                return jsonify({'result':'success'})
+        if request.method == 'GET':
+            return render_template('login.html')
+            
+        else:
+            user_id = request.form['user_id']
+            user_pw = request.form['user_pw']
+            user = User.query.filter(User.user_id==user_id).first()
+            
+            if user is not None:
+                if bcrypt.check_password_hash(user.user_pw,user_pw):
+                    session['login']=user.id
+                    return jsonify({'result':'success'})
+                else:
+                    return jsonify({'result':'fail'})
             else:
                 return jsonify({'result':'fail'})
-        else:
-            return jsonify({'result':'fail'})
 
 @board.route('/logout')
 def logout():
@@ -59,29 +59,30 @@ def logout():
 
 @board.route("/post", methods=["GET","POST"])
 def post():
-    if request.method == 'GET':
-        data = Post.query.all() #나중에 order_by(like_cnt)
-        return render_template("board.html", post_list = data)
-    else:
-        content = request.form['content']
-        author = request.form['author']
-        post = Post(author,content)
-        db.session.add(post)
-        db.session.commit()
-        return jsonify({"result":"success"})
+        if request.method == 'GET':
+            data = Post.query.all() #나중에 order_by(like_cnt)
+            now = datetime.now()
+            return render_template("board.html", post_list = data, now=now)
+        else:
+            content = request.form['content']
+            author = request.form['author']
+            post = Post(author,content)
+            db.session.add(post)
+            db.session.commit()
+            return jsonify({"result":"success"})
 
         
 @board.route("/post", methods=["DELETE"])
 def delete_post():
-    id = request.form['id']
-    author = request.form['author']
-    data = Post.query.filter(Post.id==id,Post.author==author).first()
-    if data is not None:
-        db.session.delete(data)
-        db.session.commit()
-        return jsonify({'result':'success'})
-    else:
-        return jsonify({'result':'fail'})
+        id = request.form['id']
+        author = request.form['author']
+        data = Post.query.filter(Post.id==id,Post.author==author).first()
+        if data is not None:
+            db.session.delete(data)
+            db.session.commit()
+            return jsonify({'result':'success'})
+        else:
+            return jsonify({'result':'fail'})
 
 
 @board.route("/post", methods=["PATCH"])
