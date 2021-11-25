0447ad3b25a1e8a0759e1e2930dc26390c4d0ac5
성규 김 <fredkim114@gmail.com>
2021-11-25 09:43:54 +0000

    html - 네이밍 규칙으로 앞에 visual 로 관리

--- api_visual.py
+++ api_visual.py
@@ -10,22 +10,22 @@ import base64
 from io import BytesIO
 
 #시각화
+import matplotlib.pyplot as plt
 import matplotlib.dates as md
 from matplotlib.figure import Figure
-# import seaborn as sns
+import seaborn as sns
 from bokeh.plotting import figure
 from bokeh import *
 from bokeh.embed import components
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 visual = Blueprint('visual', __name__)
 
 #가공된 18~19, 20~21년도 csv파일 ('trending_date','title','channel_title','category_id','publish_time','tags','views','likes','dislikes','comment_count','trending_ym')
-kr18 = pd.read_csv('./static/csv_file/kr18_ver2.csv')
-kr20 = pd.read_csv('./static/csv_file/kr20_ver2.csv')
-corona_info = pd.read_csv('./static/csv_file/corona_cases_ver2.csv')
-crosstab = pd.read_csv('./static/csv_file/crosstab.csv')
+kr18 = pd.read_csv('static/csv_file/kr18_ver2.csv')
+kr20 = pd.read_csv('static/csv_file/kr20_ver2.csv')
+corona_info = pd.read_csv('static/csv_file/corona_cases_ver2.csv')
+crosstab = pd.read_csv('static/csv_file/crosstab.csv')
 
 #기본 데이터 : 영상 데이터 있는것(list)  / 카테고리는 존재하지만 trending 영상은 없는 것(list2)
 category_list = {'1' : 'Film & Animation', '2' : 'Autos & Vehicles', '10' : 'Music', '15' : 'Pets & Animals', '17' : 'Sports','19' : 'Travel & Events',
@@ -35,11 +35,16 @@ category_list = {'1' : 'Film & Animation', '2' : 'Autos & Vehicles', '10' : 'Mus
 category_list2 = {'18' : 'Short Movies', '21' : 'Videoblogging', '30' : 'Movies', '31' : 'Anime/Animation', '32' : 'Action/Adventure',
                 '33' : 'Classics','34' : 'Comedy', '35' : 'Documentary', '36' : 'Drama','37' : 'Family', '38' : 'Foreign','39' : 'Horror',
                 '40' : 'Sci-Fi/Fantasy', '41' : 'Thriller', '42' : 'Shorts', '43' : 'Shows', '44' : 'Trailers'}
-
 corr = {'Travel & Events': 0.758423, 'People & Blogs':0.724855, 'Howto & Style':0.606430, 'Gaming':0.240503, 'Entertainment':0.167289, 'Science & Technology':0.112975, 'Nonprofits & Activism':0.031593, 'Music':-0.095519, 'Sports':-0.144160, 'Autos & Vehicles':-0.182005, 'Pets & Animals':-0.208127, 'Comedy':-0.219434, 'News & Politics':-0.408071, 'Film & Animation':-0.482346, 'Education':-0.649733}
 
 
-#기능1 : video_count
+
+#홈페이지 "/"
+@visual.route('/visual', methods = ['GET'])
+def home():
+    return render_template('visual_home.html')
+
+#기능1 : 카테고리 별 영상 수 변화
 @visual.route('/vid-cnt', methods = ['GET', 'POST'])
 def vidcnt():
     if request.method == 'POST':
@@ -48,7 +53,7 @@ def vidcnt():
             category_id_name = category_list[category_id]
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_video_count_home.html', category_list = category_list, msg=msg)
+            return render_template('visual_video_count_home.html', category_list = category_list, msg=msg)
         category_id = request.form['category_id']
         category_id_name = category_list[category_id]
 
@@ -78,11 +83,11 @@ def vidcnt():
         plt.savefig(buf, format='png')
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         plt.close(fig)
-        return render_template('Finished_video_count_result.html', category_id_name = category_id_name, data=data, expr_word=expr_word, change_vd_cnt=change_vd_cnt)
+        return render_template('visual_video_count_result.html', category_id_name = category_id_name, data=data, expr_word=expr_word, change_vd_cnt=change_vd_cnt)
     else:
-        return render_template('Finished_video_count_home.html', category_list = category_list)
+        return render_template('visual_video_count_home.html', category_list = category_list)
 
-#기능2: compare_mean_views
+#기능2: 평균 조회수 비교
 @visual.route('/comp-mean-views', methods = ['GET', 'POST'])
 def compare_mean_views():
     if request.method == 'POST':
@@ -91,7 +96,7 @@ def compare_mean_views():
             category_id_name = category_list[category_id]
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_compare_mean_views_home.html', category_list = category_list, msg=msg)
+            return render_template('visual_compare_mean_views_home.html', category_list = category_list, msg=msg)
         user_want_18 = kr18[kr18['category_id']==int(category_id)]
         user_want_20 = kr20[kr20['category_id']==int(category_id)]
         user_want_18 = user_want_18.drop(columns=['trending_date', 'title', 'channel_title', 'category_id', 'publish_time', 'tags', 'likes', 'dislikes', 'comment_count'])
@@ -113,11 +118,11 @@ def compare_mean_views():
         plt.savefig(buf, format='png')
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         plt.close()
-        return render_template('Finished_compare_mean_views_result.html', category_id_name=category_id_name, data=data, vari=vari)
+        return render_template('visual_compare_mean_views_result.html', category_id_name=category_id_name, data=data, vari=vari)
     else:
-        return render_template('Finished_compare_mean_views_home.html', category_list = category_list)
+        return render_template('visual_compare_mean_views_home.html', category_list = category_list)
 
-# 기능3: ratio_change_video
+# 기능3: 코로나 전후 영상들의 카테고리 순위 및 비율 변화
 @visual.route('/ratio-ch-vid', methods = ['GET', 'POST'])
 def ratio_ch_vid():
     if request.method == 'POST':
@@ -127,7 +132,7 @@ def ratio_ch_vid():
             label_num = int(label_num)
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_ratio_change_video_home.html', category_list = category_list, msg=msg)
+            return render_template('visual_ratio_change_video_home.html', category_list = category_list, msg=msg)
 
         #데이터가공(views컬럼 필요없지만 나중에 views로 통계낼 수도 있을 경우 확장성 고려)
         ver1_kr18 = kr18.iloc[:,[3,6]]
@@ -165,7 +170,7 @@ def ratio_ch_vid():
 
         colors =  ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#E3CEF6'
             ,'#FE2E2E', '#FE9A2E', '#F7FE2E', '#80FF00', '#2EFEF7', '#2E2EFE', '#D358F7']
-        plt.figure(figsize=(15,6))
+        plt.figure(figsize=(13,5))
         plt.subplot(121)
         plt.pie(top5_vals_18, labels=top5_labels_18, radius=0.9, autopct='%0.1f%%', startangle=90, colors=colors)
         plt.title('Before corona \n(2017-2018)')
@@ -179,16 +184,16 @@ def ratio_ch_vid():
         plt.savefig(buf, format='png')
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         plt.close()
-        return render_template('Finished_ratio_change_video_result.html', data=data, label_num=label_num, rank_change=rank_change, category_name=labels_20)
+        return render_template('visual_ratio_change_video_result.html', data=data, label_num=label_num, rank_change=rank_change, category_name=labels_20)
     else:
-        return render_template('Finished_ratio_change_video_home.html', category_list = category_list)
+        return render_template('visual_ratio_change_video_home.html', category_list = category_list)
 
 #기능 4: 멀티 분석(전체카테고리 or 개별카테고리 선택가능)
 @visual.route('/multi-analysis', methods = ['GET', 'POST'])
 def multi_analysis():
     youtube_col_list = ['views', 'likes', 'dislikes', 'comment_count', 'likes_ratio']
     corona_col_list = ['local_outbreak', 'imported_cases', 'death', 'total_death', 'new_cases', 'total_cases', 'variation']
-    color_list = ['r','b','g','darkorange']
+    color_list = ['darkorange','g', 'b','r']
     new_category_list={'0':'All Categories'}
     new_category_list.update(category_list)
     if request.method == 'POST':
@@ -203,9 +208,9 @@ def multi_analysis():
             category_id_name = new_category_list[category_id]
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, msg=msg, category_list=new_category_list)
+            return render_template('visual_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, msg=msg, category_list=new_category_list)
         if int(category_id) == 0:
-            #전체 카테고리인 기능 4 복붙
+            #전체 카테고리인 경우
             kr20.replace(0, np.nan)
             kr20_2 = kr20.groupby(['trending_date'], as_index=False).mean()
             def likes_per_dislikes(row):
@@ -214,7 +219,7 @@ def multi_analysis():
             kr20_2['trending_date']=pd.to_datetime(kr20_2['trending_date'], format='%Y-%m-%d')
             corona_info['date']=pd.to_datetime(corona_info['date'], format='%Y-%m-%d')
 
-            fig, ax1 = plt.subplots(figsize=(18,8))
+            fig, ax1 = plt.subplots(figsize=(13,6))
             ax2=ax1.twinx()
             selection_1 = corona_col_list[selection_1_num-1]
             selection_2 = youtube_col_list[selection_2_num-1]
@@ -232,8 +237,9 @@ def multi_analysis():
             plt.savefig(buf, format='png')
             data = base64.b64encode(buf.getbuffer()).decode("ascii")
             plt.close(fig)
-            return render_template('Finished_multi_analysis_result.html', data=data, selection_1=selection_1, selection_2=selection_2)
+            return render_template('visual_multi_analysis_result.html', data=data, selection_1=selection_1, selection_2=selection_2)
         else:
+            #개별 카테고리를 선택할 경우
             #먼저 카테고리로 추출
             category_filter = kr20[kr20['category_id']==int(category_id)]
             category_filter.replace(0, np.nan)
@@ -246,7 +252,7 @@ def multi_analysis():
             kr20_2['likes_ratio']=kr20_2.apply(lambda row: likes_per_dislikes(row), axis=1)
             kr20_2['trending_date']=pd.to_datetime(kr20_2['trending_date'], format='%Y-%m-%d')
             corona_info['date']=pd.to_datetime(corona_info['date'], format='%Y-%m-%d')
-            fig, ax1 = plt.subplots(figsize=(18,8))
+            fig, ax1 = plt.subplots(figsize=(13,6))
             ax2=ax1.twinx()
             selection_1 = corona_col_list[selection_1_num-1]
             selection_2 = youtube_col_list[selection_2_num-1]
@@ -263,16 +269,16 @@ def multi_analysis():
             plt.savefig(buf, format='png')
             data = base64.b64encode(buf.getbuffer()).decode("ascii")
             plt.close(fig)
-            return render_template('Finished_multi_analysis_category_based_result.html', data=data, selection_1=selection_1, selection_2=selection_2, category_id_name=category_id_name)
+            return render_template('visual_multi_analysis_category_based_result.html', data=data, selection_1=selection_1, selection_2=selection_2, category_id_name=category_id_name)
     else:
-        return render_template('Finished_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, category_list=new_category_list)
+        return render_template('visual_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, category_list=new_category_list)
 
 #기능 5: 코로나 관련 영상 추출후 멀티분석기능 제공(전체데이터에서 2%밖에 안되는 적은 데이터량이라 개별 카테고리 기능은 의미가없어 전체카테고리 데이터만 제공)
 @visual.route('/corona-related-multi-analysis', methods = ['GET', 'POST'])
 def corona_related_multi_analysis():
     youtube_col_list = ['views', 'likes', 'dislikes', 'comment_count', 'likes_ratio']
     corona_col_list = ['local_outbreak', 'imported_cases', 'death', 'total_death', 'new_cases', 'total_cases', 'variation']
-    color_list = ['r','b','g','darkorange']
+    color_list = ['darkorange','g', 'b','r']
     corona_related_word =['코로나', '바이러스','변이', '백신', '거리두기', '코비드', 'corona', 
                     'covid', '확진자', '사망자', '위드', '확진','마스크','전파자','방역', '자가격리', '비대면']
     corona_not_related_word =['drama', '뮤직비디오']
@@ -292,7 +298,7 @@ def corona_related_multi_analysis():
             selection_2_num = int(selection_2_num)
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_corona_related_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, category_list=category_list, corona_col_list=corona_col_list, msg=msg)
+            return render_template('visual_corona_related_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, category_list=category_list, corona_col_list=corona_col_list, msg=msg)
         kr20_1=kr20[corona_related_filter]
         kr20_1.replace(0, np.nan)
 
@@ -306,7 +312,7 @@ def corona_related_multi_analysis():
         kr20_2['trending_date']=pd.to_datetime(kr20_2['trending_date'], format='%Y-%m-%d')
         corona_info['date']=pd.to_datetime(corona_info['date'], format='%Y-%m-%d')
 
-        fig, ax1 = plt.subplots(figsize=(18,8))
+        fig, ax1 = plt.subplots(figsize=(13,6))
         ax2=ax1.twinx()
         selection_1 = corona_col_list[selection_1_num-1]
         selection_2 = youtube_col_list[selection_2_num-1]
@@ -324,9 +330,9 @@ def corona_related_multi_analysis():
         plt.savefig(buf, format='png')
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         plt.close(fig)
-        return render_template('Finished_corona_related_multi_analysis_result.html', data=data, selection_1=selection_1, selection_2=selection_2)
+        return render_template('visual_corona_related_multi_analysis_result.html', data=data, selection_1=selection_1, selection_2=selection_2)
     else:
-        return render_template('Finished_corona_related_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, category_list=category_list)
+        return render_template('visual_corona_related_multi_analysis_home.html', enumerate=enumerate, youtube_col_list=youtube_col_list, corona_col_list=corona_col_list, category_list=category_list)
 
 
 #기능 6: Bokeh 상관관계 그래프
@@ -338,7 +344,7 @@ def corona_category():
             category_id_name = category_list[category_id]
         except:
             msg = "정확한 값을 입력하세요"
-            return render_template('Finished_correlation_bokeh_home.html', category_list = category_list, msg=msg)
+            return render_template('visual_correlation_bokeh_home.html', category_list = category_list, msg=msg)
         category_id = request.form['category_id']
         category_id_name = category_list[category_id]
 
@@ -352,6 +358,6 @@ def corona_category():
         script = Markup(script)
         div = Markup(div)
 
-        return render_template('Finished_correlation_bokeh_result.html', category_id_name=category_id_name, corr=corr[category_id_name], script=script, div=div)
+        return render_template('visual_correlation_bokeh_result.html', category_id_name=category_id_name, corr=corr[category_id_name], script=script, div=div)
     else:
-        return render_template('Finished_correlation_bokeh_home.html', category_list = category_list)
+        return render_template('visual_correlation_bokeh_home.html', category_list = category_list)
