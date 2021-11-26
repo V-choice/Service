80ca46b4de8ddff0a1f34dddd3df424e30b7ff79
성규 김 <fredkim114@gmail.com>
2021-11-26 00:04:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from api import board
 from api_visual import visual
 from db_connect import db
 from flask_bcrypt import Bcrypt
-from bokeh.resources import INLINE
-from module.Beforeafter import Beforeafter
-
 
 app = Flask(__name__)
 app.register_blueprint(visual)
