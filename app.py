f74146d36a9987a4e7434e5140339be4042311df
준수 김 <rlskejdk@naver.com>
2021-11-29 22:16:10 +0900

    app.py 원상복구

--- app.py
+++ app.py
@@ -7,23 +7,16 @@ from api.api_choice import choice
 from db_connect import db
 import config
 
-def create_app():
-    
-    app = Flask(__name__)
-    app.register_blueprint(visual)
-    app.register_blueprint(board)
-    app.register_blueprint(choice)
-    app.config.from_object(config)
 
 
-    db.init_app(app)
+app = Flask(__name__)
+app.register_blueprint(visual)
+app.register_blueprint(board)
+app.register_blueprint(choice)
+app.config.from_object(config)
 
-    from models import User, Post
-    
-    # cors + migration 사용해야 할 수 도 있음.
-    return app
 
-
+db.init_app(app)
 
 if __name__=="__main__":
-    create_app().run(host='0.0.0.0',port='5000',debug=True)
+    app.run(host='0.0.0.0',port='5000',debug=True)
