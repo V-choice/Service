a4b0fa644a0b762feda219e87eba0f388aa7f956
준수 김 <rlskejdk@naver.com>
2021-11-30 05:01:31 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from secret import db_pw, secret_key
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'+db_pw+'@localhost:3306/mydb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'+db_pw+'@localhost:3306/v_choice_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # SESSION_TYPE = 'filesystem'
 SECRET_KEY = secret_key
