c71d569f7504358acacbf3ece22bd1f28a0d5356
최청현 <chunghyunccccco@gmail.com>
2021-12-01 07:57:43 +0000

    Update secret.py, .env

--- secret.py
+++ secret.py
@@ -1,3 +1,8 @@
 #나중에 gitignore에 추가해야함.
-db_pw = 'sdHL6fTZLrwn6scE' #비밀번호 변경해 줘야함.
-secret_key = 'super secret key'
+
+from dotenv import load_dotenv
+import os
+#나중에 gitignore에 추가해야함.
+load_dotenv()
+db_pw = os.environ.get("pw")
+secret_key = os.environ.get("secret")
